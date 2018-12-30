f65f86fff6b17e26d6dfd625e8578fd75488ec4f
41xu <xu_sy11111@mail.dlut.edu.cn>
2018-12-31 01:02:25 +0800

    瞎**乱改一气我现在实在是看不出来到底是哪有问题了我屁了提交先存着档吧等我清醒了再改bug

--- GA.py
+++ GA.py
@@ -23,24 +23,34 @@ def EncodingInit(populationNum, chromoLen):
     return population
 
 
-def Decoding(population, chromoLen):
+def Decoding(pop, chromoLen):
     X1 = []
     X2 = []
-    for i in range(len(population)):
+    for i in range(len(pop)):
         temp = 0
         for j in range(0,chromoLen // 2):
-            temp += population[i][j] * math.pow(2, j)
+            temp += pop[i][j] * pow(2, j)
+        # print(temp)
         X1.append(temp)
         temp = 0
         for j in range(chromoLen // 2, chromoLen):
-            temp += population[i][j] * math.pow(2, (j - chromoLen // 2))
+            temp += pop[i][j] * pow(2, j - chromoLen // 2)
+        # print(temp)
         X2.append(temp)
+    print("X1",X1)
+    print("-----")
+    print("X2",X2)
+    print("-----")
 
-    X1_ = [minX + xi * (maxX - minX) / (pow(2, chromoLen // 2) - 1) for xi in X1]
-    X2_ = [minX + xi * (maxX - minX) / (pow(2, chromoLen // 2) - 1) for xi in X2]
+    X1_ = [minX + xi * (maxX - minX) // (pow(2, chromoLen // 2) - 1) for xi in X1]
+    X2_ = [minX + xi * (maxX - minX) // (pow(2, chromoLen // 2) - 1) for xi in X2]
+
+    print("X1_",X1_)
+    print("-----")
+    print("X2_",X2_)
+    print("-----")
 
     return X1_, X2_
-
 
 def fitness1(X1, X2,population):
     # cal Y
@@ -54,9 +64,13 @@ def fitness1(X1, X2,population):
     Y = []
     for i in range(len(population)):
         Y.append(f1(X1[i], X2[i]))
+    print("Y",Y)
+    print("-----")
     maxY = max(Y)
     for i in range(len(population)):
         Y[i] = maxY - Y[i]
+    print("process Y",Y)
+    print("-----")
 
     return Y
 
@@ -67,12 +81,17 @@ def selection(population, Y):  # Roulette Wheel Selection
         probY = [y / sum(Y) for y in Y]
     else:
         return None
+    print("probY",probY)
+    print("----")
     c = 0
     for (index, item) in enumerate(probY):
         c += item
         r = random.random()
         if r < c:
-            newPopulation.append(population[index])
+            newPopulation.append([population[index]])
+    for x in newPopulation:
+        print(x)
+
     return newPopulation
 
 def crossover(newPopulation):
@@ -114,29 +133,40 @@ def mutation(newPopulation):
     return newPopulation
 
 if __name__ == '__main__':
-    totalPop=[]
-    X=[]
-    Y=[]
-    population = EncodingInit(populationNum, chromoLen)
-    totalPop.append(population)
-    for i in range(populationNum):
-        X1, X2 = Decoding(totalPop[-1], chromoLen)
-        # print(X1,X2)
-        y = fitness1(X1, X2,totalPop[-1])
-        X.append([X1,X2])
-        Y.append(y)
-        newpop = selection(totalPop[-1], y)
-        if newpop!=None:
-            newpop = crossover(newpop)
-            newpop = mutation(newpop)
-            totalPop.append(newpop)
-            # print(len(totalPop[-1]))
-        else:
+    # totalPop=[]
+    # X=[]
+    # Y=[]
+    # population = EncodingInit(populationNum, chromoLen)
+    # totalPop.append(population)
+    # for i in range(populationNum):
+
+    #     X1, X2 = Decoding(totalPop[-1], chromoLen)
+    #     # print(X1,X2)
+    #     y = fitness1(X1, X2,totalPop[-1])
+    #     X.append([X1,X2])
+    #     Y.append(y)
+    #     newpop = selection(totalPop[-1], y)
+    #     if newpop!=None:
+    #         newpop = crossover(newpop)
+    #         newpop = mutation(newpop)
+    #         totalPop.append(newpop)
+    #         # print(len(totalPop[-1]))
+    #     else:
+    #         break
+    # print("x1,x2:",X[-1])
+    # print("y:",Y[-1])
+    pop=EncodingInit(10,chromoLen)
+    count=0
+    temp=pop
+    while count<10:
+        pop=temp
+        X1,X2=Decoding(pop,chromoLen)
+        y=fitness1(X1,X2,pop)
+        ys=selection(pop,y)
+        if ys==None:
             break
-    print("x1,x2:",X[-1])
-    print("y:",Y[-1])
-
-
-
-
-
+        yc=crossover(ys)
+        ym=mutation(yc)
+        temp=ym
+        count+=1
+    print(temp)
